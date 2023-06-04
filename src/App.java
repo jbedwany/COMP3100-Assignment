64cc752ab4e97d1e4710d41185d31ca6470beecf
Justin Bedwany <justin.bedwany@students.mq.edu.au>
2023-06-04 12:00:14 +1000

    Cleaned up a bit of old code

--- src/App.java
+++ src/App.java
@@ -110,40 +110,6 @@ public class App {
                 received = receiveMessage(fromServer); 
             }
 
-            // while(!received.substring(0,4).equals("NONE")) {
-            //     if(received.substring(0,4).equals("JOBN")) {
-            //         Job currentJob = new Job(received);
-            //         switch(mode) {
-            //             case "lrr":
-            //                 // Get server details, find largest type
-            //                 if(largestServers == null) {
-            //                     largestServers = findLargestServer(toServer, fromServer);
-            //                 }
-            //                 roundRobinIndex = scheduleLRR(largestServers, fromServer, toServer, roundRobinIndex, currentJob);
-            //                 // Request next job command
-            //                 sendMessage(toServer, "REDY");
-            //                 received = receiveMessage(fromServer);
-            //                 break;
-            //             case "fc":
-            //                 scheduleFC(fromServer, toServer, currentJob);
-            //                 sendMessage(toServer, "REDY");
-            //                 received = receiveMessage(fromServer);
-            //                 break;
-            //             case "ott":
-            //                 jobsInProgress.add(currentJob);
-            //                 scheduleOTT(fromServer, toServer, currentJob);
-
-            //                 break;
-            //             default:
-            //                 System.out.println("Error with mode selection - mode " + mode + " not found");
-            //                 return;
-            //         }
-            //     } else if(received.substring(0,4).equals("JCPL")) {
-            //         sendMessage(toServer, "REDY");
-            //         received = receiveMessage(fromServer); 
-            //     }
-            // }
-
             // terminate connection
             sendMessage(toServer, "QUIT");
             received = receiveMessage(fromServer);
@@ -176,70 +142,6 @@ public class App {
             return null;
         }
     }
-
-    // static Server[] findLargestServer(DataOutputStream toServer, BufferedReader fromServer) {
-    //     sendMessage(toServer, "GETS All");
-    //     String received = receiveMessage(fromServer);
-    //     int numServers = Integer.parseInt(received.split(" ")[1]);
-    //     Server[] servers = new Server[numServers];
-    //     sendMessage(toServer, "OK");
-    //     // System.out.println("Finding largest server.");
-    //     int maxCores = 0;
-    //     String largestServer = "";
-    //     int numLargest = 0;
-        
-    //     // Identify largest type
-    //     for(int i = 0; i < numServers; i++) {
-    //         received = receiveMessage(fromServer);
-    //         servers[i] = new Server(received);
-    //         if(servers[i].cores > maxCores && servers[i].state.equals("inactive")) {
-    //             maxCores = servers[i].cores; 
-    //             largestServer = servers[i].type; 
-    //             numLargest = 0;
-    //         }
-    //         if(servers[i].type.equals(largestServer)) {
-    //             numLargest++;
-    //         }
-    //     }
-    //     // Create list of largest servers
-    //     Server[] largestServers = new Server[numLargest];
-    //     int largestServersArrayIndex = 0;                   // added to avoid looping through the new array to find the next empty element each time an element is being added
-    //     for(int i = 0; i < numServers; i++) {
-    //         if(servers[i].type.equals(largestServer) && servers[i].state.equals("inactive")) {
-    //             largestServers[largestServersArrayIndex++] = servers[i];
-    //         }
-    //     }
-    //     sendMessage(toServer, "OK");
-    //     receiveMessage(fromServer);
-    //     return largestServers;
-    // }
-
-
-    // static int scheduleLRR(Server[] largestServers, BufferedReader fromServer, DataOutputStream toServer, int roundRobinIndex, Job currentJob) {
-    //     // Schedule jobs using LRR
-    //     sendMessage(toServer, "SCHD " + currentJob.id + " " + largestServers[roundRobinIndex].type + " " + largestServers[roundRobinIndex].id);
-    //     if(++roundRobinIndex >= largestServers.length) {
-    //         roundRobinIndex = 0;
-    //     }
-    //     receiveMessage(fromServer);
-    //     return roundRobinIndex;
-    // }
-
-    // static void scheduleFC(BufferedReader fromServer, DataOutputStream toServer, Job currentJob) {
-    //         sendMessage(toServer, "GETS Capable " + currentJob.cores + " " + currentJob.mem + " " + currentJob.disk);
-    //         int numRecords = Integer.parseInt(receiveMessage(fromServer).split(" ")[1]);
-    //         sendMessage(toServer, "OK");
-    //         String received = receiveMessage(fromServer);
-    //         System.out.println(received);
-    //         for(int i = 0; i < numRecords - 1; i++) {
-    //             receiveMessage(fromServer);
-    //         }
-    //         sendMessage(toServer, "OK");
-    //         receiveMessage(fromServer);
-    //         Server firstCapable = new Server(received);
-    //         sendMessage(toServer, "SCHD " + currentJob.id + " " + firstCapable.type + " " + firstCapable.id);
-    //         receiveMessage(fromServer);
-    // }
     
     static void scheduleOTT(BufferedReader fromServer, DataOutputStream toServer, Job currentJob) {
         // Request list of servers available to complete job
